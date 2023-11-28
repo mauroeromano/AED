ec4e929d6bb6aafd2292401b0bad14cd8292f6a4
Mauro Romano <mauromano@frba.utn.edu.ar>
2023-11-28 13:00:56 -0300

    Add files via upload

---
+++ 20-Poligono/FuncionesAuxiliares.h
@@ -0,0 +1,12 @@
+// Conjunto de Operaciones (declaracion)
+double Abs(double);
+bool areNear(const double, const double, const double = 0.0001);
+
+// Conjunto de Operaciones (implementacion)
+double Abs(double x){
+    return (x>0) ? x : -x;
+}
+
+bool areNear(const double a, const double b, const double delta){
+    return Abs(a-b) < delta;
+}
